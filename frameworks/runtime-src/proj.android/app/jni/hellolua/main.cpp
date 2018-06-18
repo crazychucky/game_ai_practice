08674b5c6079fdb4d370ca350786151022a3f59b
yuanlixiao <yuanlixiao@live.cn>
2018-06-18 14:43:02 +0800

    chore：update cocos runtime-src

--- frameworks/runtime-src/proj.android/app/jni/hellolua/main.cpp
+++ frameworks/runtime-src/proj.android/app/jni/hellolua/main.cpp
@@ -22,11 +22,21 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-// stdafx.cpp : source file that includes just the standard includes
-// player.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
+#include <memory>
 
-#include "stdafx.h"
+#include <android/log.h>
+#include <jni.h>
 
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
+#include "AppDelegate.h"
+
+#define  LOG_TAG    "main"
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+
+namespace {
+std::unique_ptr<AppDelegate> appDelegate;
+}
+
+void cocos_android_app_init(JNIEnv* env) {
+    LOGD("cocos_android_app_init");
+    appDelegate.reset(new AppDelegate());
+}
